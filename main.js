f46ba475d555dc3c4451871205e439a5178b3635
Renata Silva <99251703+Itsrenata2@users.noreply.github.com>
2024-07-23 17:26:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,127 @@
+'use strict'; 
+
+const firstQuestionDiv = document.querySelector('.firstQuestion');
+const inputsOptsOne = document.getElementById('optsFirstQuestion').getElementsByTagName('input');
+const submitBtnOne = document.querySelector('.submitBtnOne');
+
+const secondQuestionDiv = document.querySelector('.secondQuestion');
+const inputsOptsTwo = document.getElementById('optsSecondQuestion').getElementsByTagName('input');
+const submitBtnTwo = document.querySelector('.submitBtnTwo');
+
+const thirdQuestionDiv = document.querySelector('.thirdQuestion');
+const inputsOptsThree = document.getElementById('optsThirdQuestion').getElementsByTagName('input');
+const submitBtnThree = document.querySelector('.submitBtnThree');
+
+const fourthQuestionDiv = document.querySelector('.fourthQuestion');
+const inputsOptsFour = document.getElementById('optsFourthQuestion').getElementsByTagName('input');
+const submitBtnFour = document.querySelector('.submitBtnFour');
+
+const message = document.querySelector('.message');
+
+const getInputValueOne = function() {
+    for (let option of inputsOptsOne) {
+        let optionValue = option.value;
+
+        if(option.checked) {
+            const checkedOpt = optionValue;
+            console.log(checkedOpt);
+            if(checkedOpt == 'a') {
+                firstQuestionDiv.classList.remove('bg-champagne');
+                firstQuestionDiv.classList.add('bg-vanilla');
+                setTimeout(function () {
+                    firstQuestionDiv.style.display = 'none';
+                    secondQuestionDiv.style.display = 'block';
+                }, 1500);
+            } else {
+                firstQuestionDiv.classList.remove('bg-champagne');
+                firstQuestionDiv.classList.add('bg-chestnut');
+                submitBtnOne.textContent = 'try again';
+                setTimeout(function () {
+                    submitBtnOne.textContent = 'submit answer';
+                }, 1500);
+            }
+        }
+    }
+} 
+submitBtnOne.addEventListener('click', getInputValueOne);
+
+const getInputValueTwo = function() {
+    for (let option of inputsOptsTwo) {
+        let optionValue = option.value;
+
+        if(option.checked) {
+            const checkedOpt = optionValue;
+            console.log(checkedOpt);
+            if(checkedOpt == 'c') {
+                secondQuestionDiv.classList.remove('bg-champagne');
+                secondQuestionDiv.classList.add('bg-vanilla');
+                setTimeout(function () {
+                    secondQuestionDiv.style.display = 'none';
+                    thirdQuestionDiv.style.display = 'block';
+                }, 1500);
+            } else {
+                secondQuestionDiv.classList.remove('bg-champagne');
+                secondQuestionDiv.classList.add('bg-chestnut');
+                submitBtnTwo.textContent = 'try again';
+                setTimeout(function () {
+                    submitBtnTwo.textContent = 'submit answer';
+                }, 1500);
+            }
+        }
+    }
+} 
+submitBtnTwo.addEventListener('click', getInputValueTwo);
+
+const getInputValueThree = function() {
+    for (let option of inputsOptsThree) {
+        let optionValue = option.value;
+
+        if(option.checked) {
+            const checkedOpt = optionValue;
+            console.log(checkedOpt);
+            if(checkedOpt == 'b') {
+                thirdQuestionDiv.classList.remove('bg-champagne');
+                thirdQuestionDiv.classList.add('bg-vanilla');
+                setTimeout(function () {
+                    thirdQuestionDiv.style.display = 'none';
+                    fourthQuestionDiv.style.display = 'block';
+                }, 1500);
+            } else {
+                thirdQuestionDiv.classList.remove('bg-champagne');
+                thirdQuestionDiv.classList.add('bg-chestnut');
+                submitBtnThree.textContent = 'try again';
+                setTimeout(function () {
+                    submitBtnThree.textContent = 'submit answer';
+                }, 1500);
+            }
+        }
+    }
+} 
+submitBtnThree.addEventListener('click', getInputValueThree);
+
+const getInputValueFour = function() {
+    for (let option of inputsOptsFour) {
+        let optionValue = option.value;
+
+        if(option.checked) {
+            const checkedOpt = optionValue;
+            console.log(checkedOpt);
+            if(checkedOpt == 'c') {
+                fourthQuestionDiv.classList.remove('bg-champagne');
+                fourthQuestionDiv.classList.add('bg-vanilla');
+                setTimeout(function () {
+                    fourthQuestionDiv.style.display = 'none';
+                    message.style.display = 'block';
+                }, 1500);
+            } else {
+                fourthQuestionDiv.classList.remove('bg-champagne');
+                fourthQuestionDiv.classList.add('bg-chestnut');
+                submitBtnFour.textContent = 'try again';
+                setTimeout(function () {
+                    submitBtnFour.textContent = 'submit answer';
+                }, 1500);
+            }
+        }
+    }
+} 
+submitBtnFour.addEventListener('click', getInputValueFour);
